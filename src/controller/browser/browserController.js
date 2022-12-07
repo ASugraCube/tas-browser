07171d06384f1a8a64a914bc63d782220f6971aa
SugraCube <87343933+ASugraCube@users.noreply.github.com>
2022-12-06 21:29:00 -0500

    hopefully it works somewhat decently now

--- src/controller/browser/browserController.js
+++ src/controller/browser/browserController.js
@@ -16,16 +16,15 @@ class BrowserController{
                 "--disable-renderer-backgrounding"
             ]
         });
+
+        this._paused=false;
     }
 
     async attach(){
         let page=(await this.browser.pages())[0];
-        let frames=(await page.$$("iframe"));
-        for(let i=0; i<frames.length; i++){
-            frames[i]=await frames[i].contentFrame();
-        }
+        let frames=page.frames();
         
-        //evaluate not awaited
+        /* TODO debug
         page.evaluate(()=>{
             let old=performance.now;
             performance.now=()=>old.call(performance)*.1;
@@ -36,13 +35,98 @@ class BrowserController{
                 performance.now=()=>old.call(performance)*.1;
             });
         });
+        */
 
-        await page.exposeFunction("onKeyDown", key=>{
-            alert(key);
+        let onKeyDown=key=>{
+            (async ()=>{
+                /*uncomment when the message passing works lmao
+                if(key==="Numpad1"){
+                    await this._pause();
+                }
+                */
+                alert(key);//TODO DEBUG
+            })();
+        };
+        try{await page.exposeFunction("onKeyDown", onKeyDown);}catch(e){}
+        let parentOrigin=await page.evaluate(()=>{
+            if(!window.isTasAttached){
+                window.isTasAttached=true;
+
+                addEventListener("keydown", e=>onKeyDown(e.code));
+                
+                let onMessage=e=>{
+                    if(e.data.message==="keydown") onKeyDown(e.data.key);
+                }
+                addEventListener("message", onMessage, false);
+            }
+
+            return origin;
         });
-        await page.evaluate(()=>{
-            window.addEventListener("keydown", e=>onKeyDown(e.code));
-        });
+        for(let i=0; i<frames.length; i++){
+            await frames[i].evaluate(parentOrigin=>{
+                if(!window.isTasAttached){
+                    window.isTasAttached=true;
+
+                    addEventListener("keydown", e=>{
+                        parent.postMessage({
+                            message: "keydown",
+                            key: e.code
+                        }, parentOrigin);
+                    });
+                }
+            }, parentOrigin);
+        }
+    }
+
+    async _pause(){
+        let page=(await this.browser.pages())[0];
+        let frames=(await page.$$("iframe"));
+        for(let i=0; i<frames.length; i++){
+            frames[i]=await frames[i].contentFrame();
+        }
+
+        if(!this._paused){
+            this._paused=true;
+
+            let dateFunc=()=>{
+                if(!window.realDateNow) window.realDateNow=Date.now;
+                let old=Date.now();
+                Date.now=()=>old;
+            };
+            let performanceFunc=()=>{
+                if(!window.realPerformanceNow) window.realPerformanceNow=performance.now;
+                let old=performance.now();
+                performance.now=()=>old;
+            };
+
+            await page.evaluate(dateFunc);
+            await page.evaluate(performanceFunc);
+
+            frames.forEach(frame=>{
+                frame.evaluate(dateFunc);
+                frame.evaluate(performanceFunc);
+            });
+        }
+        else{
+            this._paused=false;
+
+            let dateFunc=()=>{
+                let offset=Date.now()-window.realDateNow.call(Date);
+                Date.now=()=>window.realDateNow.call(Date)+offset;
+            };
+            let performanceFunc=()=>{
+                let offset=performance.now()-window.realPerformanceNow.call(performance);
+                performance.now=()=>window.realPerformanceNow.call(performance)+offset;
+            };
+
+            await page.evaluate(dateFunc);
+            await page.evaluate(performanceFunc);
+
+            frames.forEach(frame=>{
+                frame.evaluate(dateFunc);
+                frame.evaluate(performanceFunc);
+            });
+        }
     }
 }
 module.exports=new BrowserController();
